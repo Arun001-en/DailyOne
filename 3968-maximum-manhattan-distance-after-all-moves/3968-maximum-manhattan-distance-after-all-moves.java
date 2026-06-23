8f15a4489ddc8a0f3793fac43009d66240c225c1
Arun Pundhir <arunpundhir2021@gmail.com>
2026-06-23 20:55:09 +0530

    Time: 17 ms (58.02%), Space: 47.8 MB (63.77%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxDistance(String moves) {
+        int u = 0, d = 0, l = 0, r = 0, count = 0;
+        for (char c : moves.toCharArray()) {
+            if(c == 'U'){
+                u++;
+            }
+            else if(c == 'D'){
+                d++;
+            }
+            else if(c == 'L'){
+                l++;
+            }
+            else if(c == 'R'){
+                r++;
+            }
+            else{
+                count++;
+            }
+        }
+        return Math.abs(u - d) + Math.abs(l - r) + count;
+    }
+}
