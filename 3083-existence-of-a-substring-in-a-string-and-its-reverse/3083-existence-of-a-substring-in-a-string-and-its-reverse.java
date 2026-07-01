5a2b68ce8f2c1d2debd1cba1864fe0a84cd2980d
Arun Pundhir <arunpundhir2021@gmail.com>
2026-07-01 20:16:20 +0530

    Time: 5 ms (26.16%), Space: 44.4 MB (35.02%) - LeetHub

---
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubstringPresent(String s) {
+        String rev = "";
+        for(char ch : s.toCharArray()){
+            rev = ch+rev;
+        }
+        for(int i = 0 ; i<s.length()-1 ; i++){
+            if(rev.contains(s.substring(i , i+2)))
+                return true;
+        }
+        return false;
+    }
+}
