8a42e225cddf220d74d1d70282220fb4ea94f69b
Arun Pundhir <arunpundhir2021@gmail.com>
2026-07-21 18:53:18 +0530

    Time: 5 ms (43.81%), Space: 43.5 MB (19.34%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -5,16 +5,23 @@ class Solution {
         int i = 0, j = mid;
         int cl = 0;
         int rc = 0;
+        HashSet<Character> st = new HashSet<>();
+        st.add('a');
+        st.add('e');
+        st.add('i');
+        st.add('o');
+        st.add('u');
+        st.add('A');
+        st.add('E');
+        st.add('I');
+        st.add('O');
+        st.add('U');
         while (i < n / 2 && j < n) {
-            if (isVowel(s.charAt(i))) cl++;
-            if (isVowel(s.charAt(j))) rc++;
+            if (st.contains(s.charAt(i))) cl++;
+            if (st.contains(s.charAt(j))) rc++;
             i++;
             j++;
         }
         return cl == rc;
-    }
-    boolean isVowel(char ch) {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
     }
 }
