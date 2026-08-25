c0438e8b5f03af4d9fa2449adc0f07bd8e9b3408
Arun Pundhir <arunpundhir2021@gmail.com>
2026-08-25 08:10:33 +0530

    Time: 2 ms (68.2%), Space: 45.4 MB (52.43%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,13 +1,18 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
+        // Yaha hamne HashSet Banaya Usme saare element daal diye
         HashSet<Integer> s = new HashSet<>();
         for(int i : nums){
             s.add(i);
         }
+        // Yaha hamne t m k ki value daal di t = 2; for the first test case
         int t = k;
+        // Yaha hamne check kiya ki set m t matlab 2 present h ki nahi
         while(s.contains(t)){
+            // agar element present h to hum t ans k add karte rahenge
             t+=k;
         }
+        // agar element nahi h to usse direct return kar denge
         return t;
     }
 }
